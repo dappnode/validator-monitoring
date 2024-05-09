8d0961cca4ccf257562f3c60f106c6ae8fb740b4
pablomendezroyo <mendez4a@gmail.com>
2024-05-09 14:25:21 +0200

    fix minor issues

--- web3signer/signer/entrypoint.sh
+++ web3signer/signer/entrypoint.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-exec /opt/web3signer/bin/web3signer --http-host-allowlist=* \
+exec /opt/web3signer/bin/web3signer --http-listen-port=9000 \
+    --http-listen-host=0.0.0.0 --http-host-allowlist="*" \
+    eth2 \
     --slashing-protection-db-url=jdbc:postgresql://postgres.web3signer.dappnode:5432/web3signer \
     --slashing-protection-db-username=postgres \
     --slashing-protection-db-password=password \
