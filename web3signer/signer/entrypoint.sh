8c75edc8cccafb46e76bff76928c73e75c449ee9
pablomendezroyo <mendez4a@gmail.com>
2024-05-09 16:25:21 +0200

    add missing volume

--- web3signer/signer/entrypoint.sh
+++ web3signer/signer/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 exec /opt/web3signer/bin/web3signer --http-listen-port=9000 \
-    --http-listen-host=0.0.0.0 --http-host-allowlist="*" \
+    --http-listen-host=0.0.0.0 --http-host-allowlist="*" --http-cors-origins="*" \
     eth2 \
     --slashing-protection-db-url=jdbc:postgresql://postgres.web3signer.dappnode:5432/web3signer \
     --slashing-protection-db-username=postgres \
