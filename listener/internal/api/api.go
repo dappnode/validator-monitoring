be5600e25ed77779ded76c287801f1ef99278f40
Marc Font <marc.font.miralles@estudiantat.upc.edu>
2024-05-15 12:54:40 +0200

    move mongo connection to main.go

--- listener/internal/api/api.go
+++ listener/internal/api/api.go
@@ -5,28 +5,29 @@ import (
 
 	"github.com/dappnode/validator-monitoring/listener/internal/api/routes"
 	"github.com/dappnode/validator-monitoring/listener/internal/logger"
-	"github.com/dappnode/validator-monitoring/listener/internal/mongodb"
+	"go.mongodb.org/mongo-driver/mongo"
 )
 
 type httpApi struct {
 	server                   *http.Server
 	port                     string
-	dbUri                    string
+	dbClient                 *mongo.Client
+	dbCollection             *mongo.Collection
 	beaconNodeUrls           map[string]string
 	bypassValidatorFiltering bool
 }
 
 // create a new api instance
-func NewApi(port string, mongoDbUri string, beaconNodeUrls map[string]string, bypassValidatorFiltering bool) *httpApi {
+func NewApi(port string, dbClient *mongo.Client, dbCollection *mongo.Collection, beaconNodeUrls map[string]string, bypassValidatorFiltering bool) *httpApi {
 	return &httpApi{
 		port:                     port,
-		dbUri:                    mongoDbUri,
+		dbClient:                 dbClient,
+		dbCollection:             dbCollection,
 		beaconNodeUrls:           beaconNodeUrls,
 		bypassValidatorFiltering: bypassValidatorFiltering,
 	}
 }
 
-// start the api
 func (s *httpApi) Start() {
 	// if somehow s.server is not nil, it means the server is already running, this should never happen
 	if s.server != nil {
@@ -34,27 +35,12 @@ func (s *httpApi) Start() {
 	}
 
 	logger.Info("Server is running on port " + s.port)
-	var err error
 
-	// connect to the MongoDB server
-	dbClient, err := mongodb.GetMongoDbClient(s.dbUri)
-	if err != nil {
-		logger.Fatal("Failed to connect to MongoDB: " + err.Error())
-	}
-
-	// get the collection
-	dbCollection := dbClient.Database("validatorMonitoring").Collection("signatures")
-	if dbCollection == nil {
-		logger.Fatal("Failed to connect to MongoDB collection")
-	}
-
-	// setup the http api
 	s.server = &http.Server{
 		Addr:    ":" + s.port,
-		Handler: routes.SetupRouter(dbCollection, s.beaconNodeUrls, s.bypassValidatorFiltering),
+		Handler: routes.SetupRouter(s.dbCollection, s.beaconNodeUrls, s.bypassValidatorFiltering),
 	}
 
-	// start the api
 	if err := s.server.ListenAndServe(); err != nil {
 		logger.Fatal("Failed to start server: " + err.Error())
 	}
