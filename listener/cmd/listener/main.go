7f00dbc1208f951da206c8605028158cab5d2be1
Marc Font <36164126+Marketen@users.noreply.github.com>
2024-05-15 13:50:11 +0200

    move mongo connection to main.go (#41)

--- listener/cmd/listener/main.go
+++ listener/cmd/listener/main.go
@@ -4,6 +4,7 @@ import (
 	"github.com/dappnode/validator-monitoring/listener/internal/api"
 	"github.com/dappnode/validator-monitoring/listener/internal/config"
 	"github.com/dappnode/validator-monitoring/listener/internal/logger"
+	"github.com/dappnode/validator-monitoring/listener/internal/mongodb"
 	"github.com/herumi/bls-eth-go-binary/bls"
 )
 
@@ -25,9 +26,20 @@ func main() {
 		logger.Fatal("Failed to set BLS ETH mode: " + err.Error())
 	}
 
+	// Connect to MongoDB client & get the collection
+	dbClient, err := mongodb.GetMongoDbClient(config.MongoDBURI)
+	if err != nil {
+		logger.Fatal("Failed to connect to MongoDB: " + err.Error())
+	}
+	dbCollection := dbClient.Database("validatorMonitoring").Collection("signatures")
+	if dbCollection == nil {
+		logger.Fatal("Failed to connect to MongoDB collection")
+	}
+
 	s := api.NewApi(
 		config.Port,
-		config.MongoDBURI,
+		dbClient,
+		dbCollection,
 		config.BeaconNodeURLs,
 		config.BypassValidatorsFiltering,
 	)
